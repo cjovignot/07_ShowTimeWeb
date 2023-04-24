2f3cbc917113a0f8872db76897e50062853796dd
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-24 14:40:54 +0200

    Project creation : Nest & React [swagger]

---
+++ my_show_time_back/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ swagger-editor/config/jest/jest.unit.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  rootDir: path.join(__dirname, '..', '..'),
+  testEnvironment: 'jsdom',
+  testMatch: ['**/test/unit/*.js', '**/test/unit/**/*.js'],
+  setupFilesAfterEnv: ['<rootDir>/test/unit/setup.js'],
+  transformIgnorePatterns: ['/node_modules/(?!(swagger-client|react-syntax-highlighter)/)'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/test/build-artifacts/',
+    '<rootDir>/test/mocha/',
+    '<rootDir>/test/unit/setup.js',
+    '<rootDir>/test/unit/plugins/validate-semantic/validate-helper.js',
+    '<rootDir>/test/unit/mocks/ace.js'
+  ],
+};
