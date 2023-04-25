f0d2a9de760bbce4d23d38cf8e68827af5405156
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-25 15:09:09 +0200

    [FIX] app.module.ts [remove 'secret']

--- my_show_time_back/src/app.module.ts
+++ my_show_time_back/src/app.module.ts
@@ -6,7 +6,6 @@ import { AppService } from './app.service';
 
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { JwtModule } from '@nestjs/jwt';
-import { secret } from './utils/constants';
 import { join } from 'path/posix';
 
 import { ConcertModule } from './concert/concert.module';
@@ -18,14 +17,13 @@ import { TicketModule } from './ticket/ticket.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://showtime_superadmin:showttimeadmin@cluster0.sji4m3i.mongodb.net/ShowTime?retryWrites=true&w=majority'),
-    JwtModule.register({ secret, signOptions: { expiresIn: '2h' } }),
+    JwtModule.register({ signOptions: { expiresIn: '2h' } }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     ConcertModule,
     CategoryModule,
     ItemModule,UserModule,TicketModule,
 
     JwtModule.register({
-      secret,
       signOptions: { expiresIn: '2h' },
     }),
     ServeStaticModule.forRoot({
