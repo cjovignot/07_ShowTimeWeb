5c5fbddd3fe5512f607d4a89bd48b450d8bad88b
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-26 09:40:33 +0200

    [SAVE] working on fetches admin_index

--- my_show_time_back/src/main.ts
+++ my_show_time_back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.enableCors();
+  await app.listen(3000);
+  // app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 }
 bootstrap();
