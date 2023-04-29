3681f37c21062e3c98d031beb77a7fd7d1a1a353
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-29 09:25:13 +0200

    [SAVE] working on main

--- my_show_time_front/src/components/signup.js
+++ my_show_time_front/src/components/signup.js
@@ -77,81 +77,79 @@ function SignupComponent() {
           <h2 className="showtime"> Show time !! </h2>
           <h3> Sign-up for awesome concerts </h3>
           <div className="logo">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Disque_Vinyl.svg/128px-Disque_Vinyl.svg.png"
-              className="home-logo"
-              alt="logo"
-            />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Disque_Vinyl.svg/128px-Disque_Vinyl.svg.png" className="home-logo" alt="logo" />
           </div>
           <label>First name:</label>
           <br />
-          <input
-            type="text"
-            value={firstname}
-            required
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
+            <input type="text" value={firstname} required onChange={(e) => { handleChange(e); }} />
           <br />
           {/*when user write in name input box , handleChange()
 			function will be called. */}
           <label>Last name:</label>
           <br />
-          <input
-            type="text"
-            value={lastname}
-            required
-            onChange={(e) => {
-              handleLastnameChange(e);
-            }}
-          />
+            <input type="text" value={lastname} required onChange={(e) => { handleLastnameChange(e); }} />
           <br />
           {/*when user write in age input box , handleAgeChange()
 			function will be called. */}
           <label>Email:</label>
           <br />
-          <input
-            type="email"
-            value={email}
-            required
-            onChange={(e) => {
-              handleEmailChange(e);
-            }}
-          />
+            <input type="email" value={email} required onChange={(e) => { handleEmailChange(e); }} />
           <br />
           {/* when user write in email input box , handleEmailChange()
 			function will be called.*/}
           <label>Password:</label>
           <br />
-          <input
-            type="password"
-            value={password}
-            required
-            onChange={(e) => {
-              handlePasswordChange(e);
-            }}
-          />
+            <input type="password" value={password} required onChange={(e) => {handlePasswordChange(e);}}/>
           <br />
           {/* when user write in password input box ,
 				handlePasswordChange() function will be called.*/}
           <label>Confirm Password:</label>
           <br />
-          <input
-            type="password"
-            value={confPassword}
-            required
-            onChange={(e) => {
-              handleConfPasswordChange(e);
-            }}
-          />
+            <input type="password" value={confPassword} required onChange={(e) => {handleConfPasswordChange(e);}}/>
           <br />
           {/* when user write in confirm password input box ,
 					handleConfPasswordChange() function will be called.*/}
           <p className="errormessage">{errorMessage}</p>
           <input className="btn btn-active" type="submit" value="Sign-up" />
-		  
         </form>
+
+
+
+        <div className="card w-96 bg-neutral text-neutral-content">
+          
+          <div className="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Disque_Vinyl.svg/128px-Disque_Vinyl.svg.png" className="home-logo h-50 w-50"  alt="logo" />
+          </div>
+          <div className="card-body items-center text-center">
+
+            <h2 className="card-title">SIGNUP</h2>
+
+            <p>We are using cookies for no reason.</p>
+
+
+            <br />
+              <input type="text" value={firstname} placeholder="First name" required onChange={(e) => { handleChange(e); }} />
+            <br />
+            <br />
+              <input type="text" value={lastname} placeholder="Last name" required onChange={(e) => { handleLastnameChange(e); }} />
+            <br />
+            <br />
+              <input type="email" placeholder="email" value={email} required onChange={(e) => { handleEmailChange(e); }} />
+            <br />
+            <br />
+              <input type="password" placeholder="Password" value={password} required onChange={(e) => {handlePasswordChange(e);}}/>
+            <br />
+            <br />
+              <input type="password" placeholder="Confirm password" value={confPassword} required onChange={(e) => {handleConfPasswordChange(e);}}/>
+            <br />
+
+            <div className="card-actions justify-end">
+              <p className="errormessage">{errorMessage}</p>
+              <button className="btn btn-primary" type="submit" value="Sign-up">SignUp</button>
+            </div>
+
+          </div>
+        </div>
       </header>
     </div>
   );
